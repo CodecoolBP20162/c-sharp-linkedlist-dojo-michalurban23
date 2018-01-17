b5a29c906a1e55decccec526cab70317152e80a3
Michał Urban <michalurban23@gmail.com>
2018-01-17 23:44:38 +0100

    Add models for Nodes

--- src/main/java/com/codecool/michalurban/node/DoubleDirectionNode.java
+++ src/main/java/com/codecool/michalurban/node/DoubleDirectionNode.java
@@ -3,4 +3,35 @@ package com.codecool.michalurban.node;
 public class DoubleDirectionNode extends Node {
 
     private Node previous;
+
+    public DoubleDirectionNode() {
+
+    }
+
+    public DoubleDirectionNode(Object data) {
+
+        super(data);
+    }
+
+    public DoubleDirectionNode(Object data, Node previous) {
+
+        super(data);
+        this.previous = previous;
+    }
+
+    public DoubleDirectionNode(Object data, Node next, Node previous) {
+
+        super(data, next);
+        this.previous = previous;
+    }
+
+    public Node getPrevious() {
+
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+
+        this.previous = previous;
+    }
 }
--- src/main/java/com/codecool/michalurban/node/SingleDirectionNode.java
+++ src/main/java/com/codecool/michalurban/node/SingleDirectionNode.java
@@ -2,5 +2,17 @@ package com.codecool.michalurban.node;
 
 public class SingleDirectionNode extends Node {
 
+    public SingleDirectionNode() {
 
+    }
+
+    public SingleDirectionNode(Object data) {
+
+        super(data);
+    }
+
+    public SingleDirectionNode(Object data, Node next) {
+
+        super(data, next);
+    }
 }
